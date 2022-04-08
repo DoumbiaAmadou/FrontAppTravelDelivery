d76546c9d2a84d702c802189a16cff9a1758c4d8
amadou <doumbia.amado@gmail.com>
2022-04-08 19:47:31 +0200

    fix router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import NavBar from "./Navbar";
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from "./home";
+import './App.css' ; 
 import Contact from "./Contact";
 import About from "./About";
+import Profile  from "./Profile";
 
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
       <div className="App">
         <NavBar></NavBar>
         <Route exact path='/' component={Home} />
+        <Route path='/profile' component={Profile} />
         <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
       </div>
--- src/home.jsx
+++ src/home.jsx
@@ -5,19 +5,23 @@ const Home = () => {
     <div>
       <div className="container">
         <h4 className="center"> trajets dispoible dans les jours à venir  </h4>
-        <p>
-          <ul class="collection">
-            <li class="collection-item avatar">
+        <p />
+          <ul className="collection">
+            <li className="collection-item avatar">
               <a href="#!">
-                <i class="material-icons">directions_transit</i>
+                <i className="material-icons">directions_transit</i>
                 Paris
-                <i class="material-icons">send</i>  New York <span>  _____  </span> 22/10/2021   _____  22H </a></li>
-            <li href="#!" class="collection-item avatar"><a href="#!"> <i class="material-icons">directions_transit</i> Paris  <i class="material-icons">send</i>  New York <span>  _____  </span> 22/10/2021 22H </a></li>
-            <li href="#!" class="collection-item avatar"><a href="#!"> <i class="material-icons">directions_transit</i> Paris  <i class="material-icons">send</i>  New York <span>  _____  </span> 22/10/2021  22H </a></li>
+                <i className="material-icons">send</i>  New York <span>  _____  </span> 22/10/2021   _____  22H </a></li>
+            <li href="#!" className="collection-item avatar">
+              <a href="#!"> <i className="material-icons">directions_transit</i> Paris  
+              <i className="material-icons">send</i>  New York 
+              <span>  _____  </span> 22/10/2021 22H </a></li>
+            <li href="#!" className="collection-item avatar">
+              <a href="#!"> <i className="material-icons">directions_transit</i> Paris  
+              <i className="material-icons">send</i>  New York 
+              <span>  _____  </span> 22/10/2021  22H </a></li>
 
           </ul>
-        </p>
-
       </div>
     </div>
   )
