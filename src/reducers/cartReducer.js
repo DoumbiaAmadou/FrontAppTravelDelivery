f88ad400a9f25b0d8d92a0204c3cae9cfee6c6c6
amadou <doumbia.amado@gmail.com>
2022-11-09 01:09:08 +0100

    update Complexity of cart Updating

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,62 +1,61 @@
 let initialAuth = [];
 let getLocalAuth = () => {
-  let cart = JSON.parse(sessionStorage.getItem('CART'));
+  let cart = JSON.parse(sessionStorage.getItem("CART"));
 
-  console.log('CART', cart);
+  console.log("CART", cart);
   if (cart) {
     return cart;
   } else {
-    sessionStorage.setItem('CART', JSON.stringify(initialAuth))
+    sessionStorage.setItem("CART", JSON.stringify(initialAuth));
     return initialAuth;
   }
 };
 
-
-export const UPDATE_RESERVATION = 'UPDATE_RESERVATION'
-export const ADD_RESERVATION = 'ADD_RESERVATION';
-export const REMOVE_RESERVATION = 'REMOVE_RESERVATION';
-export const VALIDATE_RESERVATION = 'VALIDATE_RESERVATION';
-
+export const UPDATE_RESERVATION = "UPDATE_RESERVATION";
+export const ADD_RESERVATION = "ADD_RESERVATION";
+export const REMOVE_RESERVATION = "REMOVE_RESERVATION";
+export const VALIDATE_RESERVATION = "VALIDATE_RESERVATION";
 
 function cartReducer(state = getLocalAuth(), action) {
   let newCart;
   switch (action.type) {
-
     case ADD_RESERVATION:
-      //TODO: add new Cart By ID. 
-      let isAlreadyInCart = state.filter(element => element._id === action.payload._id);
-      if (isAlreadyInCart.length > 0) {
-        newCart = state.map(element => (element._id === action.payload._id) ? {
-          ...isAlreadyInCart[0],
-          quantity: isAlreadyInCart[0].quantity + action.payload.quantity
-        } : element)
+      //TODO: add new Cart By ID.
+      let isUpdated = false;
+      newCart = state.map((element) => {
+        if (element._id === action.payload._id) {
+          isUpdated = true;
+          return {
+            ...element,
+            quantity: element.quantity + action.payload.quantity,
+          };
+        } else return element;
+      });
 
-      } else {
-        newCart = [...state, action.payload]
+      if (!isUpdated) {
+        newCart = [...newCart, action.payload];
       }
 
-      sessionStorage.setItem('CART', JSON.stringify(newCart));
-      return newCart; // payload eg: {tipId: 00000, kilosReserved:4 , kiloReservedPrice: 10, priceTotal : 80 , date_Res: 11/11/2022, } 
-    case UPDATE_RESERVATION:
-      //TODO: add new Cart By ID. 
-      let findedElement = state.filter(element => element._id === action.payload._id);
-      if (findedElement.length > 0) {
-        newCart = state.map(element => (element._id === action.payload._id) ? {
-          ...findedElement[0],
-          quantity: action.payload.quantity
-        } : element)
-
-      } else {
-        newCart = [...state, action.payload]
-      }
-
+      sessionStorage.setItem("CART", JSON.stringify(newCart));
       return newCart;
+
+    case UPDATE_RESERVATION:
+      newCart = state.map((element) =>
+        element._id === action.payload._id
+          ? {
+              ...element,
+              quantity: action.payload.quantity,
+            }
+          : element
+      );
+      return newCart;
+
     case REMOVE_RESERVATION:
-      newCart = state.filter((element) => element !== action.payload);
-      sessionStorage.setItem('CART', JSON.stringify(newCart));
+      newCart = state.filter((element) => element._id !== action.payload._id);
+      sessionStorage.setItem("CART", JSON.stringify(newCart));
       return newCart;
     default:
-      return state
+      return state;
   }
 }
 
